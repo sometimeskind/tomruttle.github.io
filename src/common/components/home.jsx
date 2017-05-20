4977bbb5f2f62dc66b8568aa9d46f31434cf1905
Tom Ruttle <tom@tomruttle.com>
2017-05-21 00:21:38 +0300

    more wording changes

--- src/common/components/home.jsx
+++ src/common/components/home.jsx
@@ -6,11 +6,11 @@ function Home() {
   return (
     <div>
       <h4>Home</h4>
-      <p>When this site was a blog I never wrote anything on it, so now it&apos;s just somewhere I&apos;m going to play with fun technology. I&apos;m also building it from scratch rather than using a UI framework, which is why it looks a little bare.</p>
+      <p>When this site was a blog I never wrote anything on it, so now it&apos;s just somewhere I&apos;m going to play with fun technology. I&apos;m also building it from scratch rather than using a UI framework, which is why it looks a little spare.</p>
       <p>To render its &lt;10 views, this static SPA so far uses:</p>
       <ul>
         <li>Isomorphic React/React-Router</li>
-        <li>Webpack, in particular this really excellent module called <a href="https://github.com/markdalgleish/static-site-generator-webpack-plugin" rel="noopener noreferrer" target="_blank">static-site-generator</a> that allows me to serve this site statically/statelessly from Github Pages for free</li>
+        <li>Webpack, in particular a really excellent plugin called <a href="https://github.com/markdalgleish/static-site-generator-webpack-plugin" rel="noopener noreferrer" target="_blank">static-site-generator</a> that allows me to serve this site statically/statelessly from Github Pages for free</li>
         <li>Automatic loading/reading/parsing/inserting of Markdown files as posts</li>
         <li>Flow typing</li>
         <li>Travis CI, which rebuilds the static assets on git push</li>
