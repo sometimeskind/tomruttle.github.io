d178dea8c115764e0cdf1789a392dee2dc5da442
Tom Ruttle <tom@tomruttle.com>
2017-05-21 20:36:17 +0300

    open external links in new tab

--- src/common/components/footer.jsx
+++ src/common/components/footer.jsx
@@ -7,8 +7,8 @@ export default class Footer extends PureComponent {
     return (
       <footer className="pure-u-1 offset-md-2-5 pure-u-md-3-5">
         <div className="footer-content">
-          <a href="https://twitter.com/tomruttle" className="twitter-button">Twitter: @tomruttle</a>
-          <a href="https://github.com/tomruttle" className="github-button">Github: @tomruttle</a>
+          <a href="https://twitter.com/tomruttle" className="twitter-button" target="_blank" rel="noopener noreferrer">Twitter: @tomruttle</a>
+          <a href="https://github.com/tomruttle" className="github-button" target="_blank" rel="noopener noreferrer">Github: @tomruttle</a>
         </div>
       </footer>
     );
