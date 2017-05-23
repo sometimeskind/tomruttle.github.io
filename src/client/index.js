089b3d666937c10c9145deb6218d31aa8003debe
Tom Ruttle <tom@tomruttle.com>
2017-05-23 20:56:54 +0300

    handle promise errors

--- src/client/index.js
+++ src/client/index.js
@@ -26,6 +26,10 @@ render((
   console.log('app rendered');
 
   if ('serviceWorker' in window.navigator) {
-    window.addEventListener('load', () => window.navigator.serviceWorker.register('/sw.js'));
+    window.addEventListener('load', () => {
+      window.navigator.serviceWorker.register('/sw.js')
+        .then((registration) => { console.log('ServiceWorker registration successful with scope: ', registration.scope); })
+        .catch((err) => { console.log('ServiceWorker registration failed: ', err); });
+    });
   }
 });
