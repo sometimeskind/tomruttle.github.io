64b9289eb3681213851db22dab8b32cf9f6a29c0
Tom Ruttle <tom@tomruttle.com>
2017-06-10 19:48:25 +0100

    get path from routes

--- src/common/components/header/header.jsx
+++ src/common/components/header/header.jsx
@@ -3,16 +3,17 @@
 import React from 'react';
 import pathToRegexp from 'path-to-regexp';
 
-import type { SiteRoutes } from '../../types';
+import type { SiteRoutes, SiteRoute } from '../../types';
 
 import { PaddedHeader, TitleLink, HeaderList, HeaderListItem, HeaderListLink, activeClassName } from './header.styles';
 
 export default function Header({ routes }: { routes: SiteRoutes }) {
   const links = routes.filter((route) => route.get('title'));
+  const homeRoute: SiteRoute = links.filter((route) => route.get('key') === 'home').get(0);
 
   return (
     <PaddedHeader className="pure-u-1 pure-u-md-3-4 offset-md-1-4">
-      <h2><TitleLink to="/">HELLO</TitleLink></h2>
+      <h2><TitleLink to={homeRoute.get('path')}>HELLO</TitleLink></h2>
 
       <nav>
         <HeaderList>
