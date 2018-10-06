710311dc9229a0d7825f584db5429faf5fbb5b94
Tom Ruttle <tom@tomruttle.com>
2018-10-06 23:34:10 +0200

    have the serviceWorker return content if offline

--- sw.js
+++ sw.js
@@ -1,21 +1,55 @@
+const CACHE_NAME = 'tomruttle';
+
 const assets = [
   '/',
   '/index.html',
 ];
 
-self.addEventListener('install', (e) => {
-  const addAssetsToCache = caches
-   .open('tomruttle')
-   .then((cache) => cache.addAll(assets));
+const fallbackResponse = /* @html */`
+  <div class="noscript">
+    <p>Hi.</p>
+    <p>There's a github gist here with some words in it that you can't see because you're offline.</p>
+    <p>You're not missing anything.</p>
+    <p>🦔</p>
+  </div>
+`;
 
- e.waitUntil(addAssetsToCache);
+async function addAssetsToCache(e) {
+  const cache = await caches.open(CACHE_NAME);
+
+  await cache.addAll(assets);
+}
+
+async function fetchAsset(e) {
+  const cache = await caches.open(CACHE_NAME);
+
+  const cachedResponse = await cache.match(e.request);
+
+  if (cachedResponse) {
+    e.waitUntil(async () => {
+      await cache.add(e.request);
+    });
+
+    return cachedResponse;
+  }
+
+  try {
+    return await fetch(e.request);
+  } catch (err) {
+    if (e.request.url.includes('https://gist.github.com/tomruttle')) {
+      return new Response(`document.write(\`${fallbackResponse}\`)`, {
+        headers: {'Content-Type': 'application/javascript'}
+      });
+    }
+  }
+}
+
+addEventListener('install', (e) => {
+  e.waitUntil(addAssetsToCache(e));
 });
 
-self.addEventListener('fetch', (e) => {
-  const fetchResource = caches
-    .match(e.request)
-    .then((response) => response || fetch(e.request))
-    .catch((err) => { console.error(err); });
-
-  e.respondWith(fetchResource);
+addEventListener('fetch', (e) => {
+  if (e.request.method === 'GET') {
+    e.respondWith(fetchAsset(e));
+  }
 });
