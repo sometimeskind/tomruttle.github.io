4ad8f33c2fb8ba950bed4cf942f15faa3b24af1f
Tom Ruttle <tom@tomruttle.com>
2018-10-06 23:54:48 +0200

    move fallback content to own file

--- sw.js
+++ sw.js
@@ -3,16 +3,8 @@ const CACHE_NAME = 'tomruttle';
 const assets = [
   '/',
   '/index.html',
+  '/fallback-content.js',
 ];
-
-const fallbackResponse = /* @html */`
-  <div class="noscript">
-    <p>Hi.</p>
-    <p>There's a github gist here with some words in it that you can't see because you're offline.</p>
-    <p>You're not missing anything.</p>
-    <p>🦔</p>
-  </div>
-`;
 
 async function addAssetsToCache(e) {
   const cache = await caches.open(CACHE_NAME);
@@ -26,10 +18,6 @@ async function fetchAsset(e) {
   const cachedResponse = await cache.match(e.request);
 
   if (cachedResponse) {
-    e.waitUntil(async () => {
-      await cache.add(e.request);
-    });
-
     return cachedResponse;
   }
 
@@ -37,9 +25,7 @@ async function fetchAsset(e) {
     return await fetch(e.request);
   } catch (err) {
     if (e.request.url.includes('https://gist.github.com/tomruttle')) {
-      return new Response(`document.write(\`${fallbackResponse}\`)`, {
-        headers: {'Content-Type': 'application/javascript'}
-      });
+      return cache.match('/fallback-content.js');
     }
   }
 }
