6396f9517c36e016a11c81f604bcd6955950da24
Tom Ruttle <tom@tomruttle.com>
2018-10-07 00:02:08 +0200

    move fallback response back to sw

--- fallback-content.js
+++
@@ -1,8 +0,0 @@
-document.write(/* @html */`
-  <div class="noscript">
-    <p>Hi.</p>
-    <p>There should be a github gist here with some words in it, but you can't see because you're offline.</p>
-    <p>You're not missing anything.</p>
-    <p>🦔</p>
-  </div>
-`);
--- sw.js
+++ sw.js
@@ -3,8 +3,18 @@ const CACHE_NAME = 'tomruttle';
 const assets = [
   '/',
   '/index.html',
-  '/fallback-content.js',
 ];
+
+const fallbackResponse = /* @html */`
+document.write(\`
+  <div class="noscript">
+    <p>Hi.</p>
+    <p>There should be a github gist here with some words in it, but you can't see because you're offline.</p>
+    <p>You're not missing anything.</p>
+    <p>🦔</p>
+  </div>
+\`);
+`;
 
 async function addAssetsToCache(e) {
   const cache = await caches.open(CACHE_NAME);
@@ -25,7 +35,9 @@ async function fetchAsset(e) {
     return await fetch(e.request);
   } catch (err) {
     if (e.request.url.includes('https://gist.github.com/tomruttle')) {
-      return cache.match('/fallback-content.js');
+      return new Response(fallbackResponse, {
+        headers: {'Content-Type': 'application/javascript'}
+      })
     }
   }
 }
